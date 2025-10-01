aeb2dfced492f77dcbae423fd66f944fa7d910f2
Aniruddh Galgali <anirgalgali@gmail.com>
2025-10-01 13:39:59 +0100

    reorganized training scripts

--- min_llm/train.py
+++ min_llm/train.py
@@ -1,17 +1,14 @@
 import os
-import argparse
 import math
-import pprint
 import numpy as np
 import torch
 from torch.nn.functional import cross_entropy
-from .config import SelfAttentionConfig, TransformerConfig,DecoderLMConfig, RunConfig, TrainingConfig
-from .models.causal_llm import TransformerLM
 from torch.utils.data import DataLoader
 from torch.optim import Optimizer, AdamW
 from torch.nn.utils import clip_grad_norm_
 from torch.optim.lr_scheduler import LRScheduler
-# from torchmetrics.text import Perplexity
+from .config import SelfAttentionConfig, TransformerConfig,DecoderLMConfig, RunConfig, TrainingConfig
+from .models.causal_llm import TransformerLM
 from .data import get_batch, SequenceDataset
 from .scheduler import get_cosine_schedule_with_warmup
 from tqdm.auto import tqdm
@@ -282,9 +279,8 @@ class Trainer:
             wandb.finish()
 
         return self.model, final_eval_results
-
-
-def run(args):
+    
+def train_model(args):
 
     ## Loading data from disk
 
@@ -294,6 +290,7 @@ def run(args):
         raise e
 
     print("Mapped data from disk")
+
     ## Creating the configs
     train_config = TrainingConfig(learning_rate=args.lr,
                                   weight_decay=args.weight_decay,
@@ -382,106 +379,3 @@ def run(args):
     final_model, final_eval_results = trainer.train()
 
     return final_model, final_eval_results
-
-
-if __name__ == "__main__":
-
-    parser = argparse.ArgumentParser(description="Train causalLM on TinyStories")
-
-    parser.add_argument( "--data_dir", type=str, 
-                        default="./data/tokenized/tinystories", help="Directory containing data .npy files.")
-    
-    parser.add_argument( "--ckpt_dir", type=str, 
-                        default="./checkpoints/tinystories",
-                        help="Directory to store model checkpoints.")
-    
-    parser.add_argument( "--lr", type=float, 
-                        default=0.0005, 
-                        help="max learning rate")
-    
-    parser.add_argument( "--weight_decay", type=float, 
-                        default=0.01, 
-                        help="weight decay rate for adam")
-    
-    parser.add_argument( "--beta1", type=float, 
-                        default=0.90, 
-                        help="beta1 for adam")
-    
-    parser.add_argument( "--beta2", type=float, 
-                        default=0.95, 
-                        help="beta2 for adam")
-    
-    parser.add_argument( "--num_iters", type=int, 
-                        default=2500, 
-                        help="number of training iterations")
-    
-    parser.add_argument( "--patience_threshold", type=int, 
-                        default=3, 
-                        help="patience for early stopping")
-    
-    parser.add_argument( "--batch_size", type=int,
-                        default=512, 
-                        help="minibatch size for training")
-    
-    parser.add_argument( "--resume_from_ckpt", type=str, 
-                        default=None, 
-                        help="ckpt path to load for resuming training")
-    
-    parser.add_argument( "--reset_sched_on_load", action='store_true',
-                        help="flag that specifies whether to reset scheduler state")
-    
-    parser.add_argument( "--eval_n_steps", type = int,
-                        default=500,
-                        help="frequency at which to evaluate")
-    
-    parser.add_argument( "--frac_warmup_steps", type=float, 
-                        default=0.05, 
-                        help="fraction of traning steps to use for warmup")
-    
-    parser.add_argument( "--frac_cosine_steps", type=float, 
-                        default=0.95, 
-                        help="fraction of traning steps to use for warmup + cosine schedule")
-    
-    parser.add_argument( "--d_model", type=int, 
-                        default=512, 
-                        help="model dimensionality")
-    
-    parser.add_argument( "--vocab_size", type=int, 
-                        default=50257, # standard for gpt-2 tokenizer
-                        help="size of vocabulary (depends on tokenizer)")
-    
-    parser.add_argument( "--ctx_len", type=int, 
-                        default=256, 
-                        help="maximum context length")
-    
-    parser.add_argument( "--n_layers", type=int, 
-                        default=4, 
-                        help="number of layers")
-    
-    parser.add_argument( "--n_heads", type=int, 
-                        default=16, 
-                        help="number of attention heads")
-    
-    parser.add_argument( "--pos_embed", type=str, 
-                        default="rope", 
-                        help="type of position embedding")
-    
-    parser.add_argument( "--dropout", type=float, 
-                        default=0.1, 
-                        help="dropout")
-    
-    parser.add_argument( "--dropout_attn", type=float, 
-                        default=0.1, 
-                        help="dropout for self attention")
-    
-    parser.add_argument( "--norm_type", type=str, 
-                        default="rms", 
-                        help="type of nomr layer")
-    
-    parser.add_argument( "--norm_pos", type=str, 
-                        default="pre", 
-                        help="position of norm layer")
-    
-
-    args = parser.parse_args()
-    final_model, final_eval_results = run(args)
---
+++ scripts/train_tinystories.py
@@ -0,0 +1,112 @@
+import argparse
+from min_llm.train import train_model
+
+def main():
+
+    parser = argparse.ArgumentParser(description="Train causalLM on TinyStories")
+
+    parser.add_argument( "--data_dir", type=str, 
+                        default="../data/tokenized/tinystories", help="Directory containing data .npy files.")
+    
+    parser.add_argument( "--ckpt_dir", type=str, 
+                        default="../experiments/tinystories/checkpoints",
+                        help="Directory to store model checkpoints.")
+    
+    parser.add_argument( "--lr", type=float, 
+                        default=0.0005, 
+                        help="max learning rate")
+    
+    parser.add_argument( "--weight_decay", type=float, 
+                        default=0.01, 
+                        help="weight decay rate for adam")
+    
+    parser.add_argument( "--beta1", type=float, 
+                        default=0.90, 
+                        help="beta1 for adam")
+    
+    parser.add_argument( "--beta2", type=float, 
+                        default=0.95, 
+                        help="beta2 for adam")
+    
+    parser.add_argument( "--num_iters", type=int, 
+                        default=2500, 
+                        help="number of training iterations")
+    
+    parser.add_argument( "--patience_threshold", type=int, 
+                        default=3, 
+                        help="patience for early stopping")
+    
+    parser.add_argument( "--batch_size", type=int,
+                        default=512, 
+                        help="minibatch size for training")
+    
+    parser.add_argument( "--resume_from_ckpt", type=str, 
+                        default=None, 
+                        help="ckpt path to load for resuming training")
+    
+    parser.add_argument( "--reset_sched_on_load", action='store_true',
+                        help="flag that specifies whether to reset scheduler state")
+    
+    parser.add_argument( "--eval_n_steps", type = int,
+                        default=500,
+                        help="frequency at which to evaluate")
+    
+    parser.add_argument( "--frac_warmup_steps", type=float, 
+                        default=0.05, 
+                        help="fraction of traning steps to use for warmup")
+    
+    parser.add_argument( "--frac_cosine_steps", type=float, 
+                        default=0.95, 
+                        help="fraction of traning steps to use for warmup + cosine schedule")
+    
+    parser.add_argument( "--d_model", type=int, 
+                        default=512, 
+                        help="model dimensionality")
+    
+    parser.add_argument( "--vocab_size", type=int, 
+                        default=50257, # standard for gpt-2 tokenizer
+                        help="size of vocabulary (depends on tokenizer)")
+    
+    parser.add_argument( "--ctx_len", type=int, 
+                        default=256, 
+                        help="maximum context length")
+    
+    parser.add_argument( "--n_layers", type=int, 
+                        default=4, 
+                        help="number of layers")
+    
+    parser.add_argument( "--n_heads", type=int, 
+                        default=16, 
+                        help="number of attention heads")
+    
+    parser.add_argument( "--pos_embed", type=str, 
+                        default="rope", 
+                        help="type of position embedding")
+    
+    parser.add_argument( "--dropout", type=float, 
+                        default=0.1, 
+                        help="dropout")
+    
+    parser.add_argument( "--dropout_attn", type=float, 
+                        default=0.1, 
+                        help="dropout for self attention")
+    
+    parser.add_argument( "--norm_type", type=str, 
+                        default="rms", 
+                        help="type of nomr layer")
+    
+    parser.add_argument( "--norm_pos", type=str, 
+                        default="pre", 
+                        help="position of norm layer")
+    
+
+    args = parser.parse_args()
+    final_model, final_eval_results = train_model(args)
+    return final_model, final_eval_results
+
+
+if __name__ == "__main__":
+
+    final_model, final_eval_results = main()
+
+    
